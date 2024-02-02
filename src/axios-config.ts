65e81e4fd4a8ff56bb357e38bdc4d9602ea6a823
Allen <d4rkmage80@gmail.com>
2024-02-02 18:49:46 -0500

    changed localhost to heroku host

--- src/axios-config.ts
+++ src/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://tyche-84788637e4fc.herokuapp.com',
     withCredentials: true
 });
 
