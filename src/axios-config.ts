7a5f8583d32d60f3392ba9c69689e4ff21bb92f6
Allen <d4rkmage80@gmail.com>
2024-02-03 17:56:52 -0500

    changing env var to vite format

--- src/axios-config.ts
+++ src/axios-config.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+//@ts-ignore
 const client = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 });
 
