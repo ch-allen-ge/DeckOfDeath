88e24860f6fca69bcd3a291041777fc4389d765b
Allen Tan <d4rkmage80@gmail.com>
2024-03-04 13:53:38 -0500

    made saved workouts into cards

--- src/Pages/SavedWorkoutsPage/SavedWorkoutsPage.tsx
+++ src/Pages/SavedWorkoutsPage/SavedWorkoutsPage.tsx
@@ -2,9 +2,7 @@ import { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../hooks";
 import { dodGet } from "../../axios-config";
 import { setSavedWorkouts } from "../../reduxSlices/profileSlice";
-import WorkoutDisplay from "../../components/WorkoutDisplay";
 import './savedWorkoutsPageStyles.scss';
-import { Button } from "@mui/material";
 import { 
     setClubsExercise,
     setDiamondsExercise,
@@ -18,6 +16,9 @@ from "../../reduxSlices/exercisesChosenSlice";
 import { setBreakoutAces } from "../../reduxSlices/workoutOptionsSlice";
 import { useNavigate } from "react-router-dom";
 import LoginRegisterPage from "../LoginReigsterPage";
+import Button from "../../components/Button";
+import { setUsername } from "../../reduxSlices/userSlice";
+import { setLoggedIn } from "../../reduxSlices/UISlice";
 
 interface WorkoutInterface {
     name: string,
@@ -39,13 +40,28 @@ const SavedWorkoutPage = () => {
     const isLoggedIn = useAppSelector((state) => state.UI.loggedIn);
 
     useEffect(() => {
-        const getCustomWorkouts = async () => {
-          const response = await dodGet('/workouts/getCustomWorkouts');
-          const savedWorkouts = response.data;
-          dispatch(setSavedWorkouts(savedWorkouts));
-        }
-        
-        getCustomWorkouts();
+        const checkUserLoggedIn = async () => {
+            try {
+                const response = await dodGet('/authenticateToken');
+    
+                if (response && response.status === 200) {
+                    const getCustomWorkouts = async () => {
+                        const response = await dodGet('/workouts/getCustomWorkouts');
+                        const savedWorkouts = response.data;
+                        dispatch(setSavedWorkouts(savedWorkouts));
+                    }
+    
+                    dispatch(setLoggedIn(true));
+                    getCustomWorkouts();
+                } else {
+                    dispatch(setLoggedIn(false));
+                }
+            } catch (e) {
+            
+            }
+        };
+
+        checkUserLoggedIn();
     }, []);
 
     const handleClick = (selectedWorkout: WorkoutInterface) => {
@@ -64,15 +80,41 @@ const SavedWorkoutPage = () => {
     return (
         <div className="savedWorkoutsPageContainer">
             {isLoggedIn ? 
-                <div className='savedWorkoutsContainer'> 
+                <div className='savedWorkoutsContainer'>
+                    <div className="savedWorkoutsText">
+                        Saved Workouts
+                    </div>
                     <div className="rowDisplay">
                         {savedWorkoutsArray.map((workout: WorkoutInterface, index) => 
-                            <div className="savedWorkoutContainer" key={index}>
-                                <div>{workout.name}</div>
-                                <WorkoutDisplay workout={workout} index={index} />
-                                <Button onClick={() => handleClick(workout)}>
-                                    Start
-                                </Button>
+                            <div className="card">
+                                <div className="card__side card__side--front">
+                                    {/* <div className="card__picture card__picture--1"></div> */}
+                                    <span className="card__heading">
+                                        <span className="card__heading-span card__heading-span--1">
+                                            {workout.name}
+                                        </span>
+                                    </span>
+                                    <div className="card__details">
+                                        <span>{workout.clubs_exercise}</span>
+                                        <span>{workout.diamonds_exercise}</span>
+                                        <span>{workout.hearts_exercise}</span>
+                                        <span>{workout.spades_exercise}</span>
+                                        {workout.breakout_aces &&
+                                            <span>
+                                                {workout.aces_exercise} {workout.timer_used && `for ${workout.aces_minutes_to_do} min ${workout.aces_seconds_to_do} sec`}
+                                            </span>
+                                        }
+                                    </div>
+                                </div>
+                                <div className="card__side card__side--back card__side--back-1">
+                                    <div className="card__cta">
+                                        <Button
+                                            onClick={() => handleClick(workout)}
+                                        >
+                                            Start
+                                        </Button>
+                                    </div>
+                                </div>
                             </div>
                         )}
                     </div>
