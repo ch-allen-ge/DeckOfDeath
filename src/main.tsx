cd49d9dcf1ba72c49d849a2aa2410a453e781ea8
Allen Tan <d4rkmage80@gmail.com>
2024-03-11 12:30:54 -0400

    dont render navbar on error page, compressed coach images

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import Navbar from './components/Navbar/Navbar.tsx';
 import {
     BrowserRouter,
     Routes,
-    Route
+    Route,
+    useMatch
 } from "react-router-dom";
 import { AuthProvider } from './auth/AuthContext.tsx';
 import {
@@ -16,35 +17,60 @@ import {
 import './index.scss';
 
 const HomePage = lazy(() =>  import('./Pages/HomePage'));
-const ErrorPage = lazy(() => import('./Pages/ErrorPage'));
 const WorkoutPage = lazy(() => import('./Pages/WorkoutPage'));
 const RegisterPage = lazy(() => import('./Pages/RegisterPage'));
 const LoginPage = lazy(() =>  import('./Pages/LoginPage'));
 const ProfilePage = lazy(() => import('./Pages/ProfilePage'));
 const CoachPage = lazy(() => import('./Pages/CoachPage'));
 const SavedWorkoutsPage = lazy(() => import('./Pages/SavedWorkoutsPage'));
+const ErrorPage = lazy(() => import('./Pages/ErrorPage'));
 const queryClient = new QueryClient();
 
+const HideOnErrorPage = ({children} : {children : React.ReactNode}) => {
+    const onHomePage = useMatch('/');
+    const onWorkoutPage = useMatch('/workout');
+    const onRegisterPage = useMatch('/register');
+    const onLoginPage = useMatch('login');
+    const onProfilePage = useMatch('/profile');
+    const onCoachPage = useMatch('/coach');
+    const onSavedWorkoutsPage = useMatch('savedWorkouts');
+
+    if (onHomePage || onWorkoutPage || onRegisterPage || onLoginPage || onProfilePage || onCoachPage || onSavedWorkoutsPage) {
+        return children;
+    } else {
+        return null;
+    }
+};
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                <BrowserRouter>
+                    <Suspense>
+                        <HideOnErrorPage>
+                            <Navbar />
+                        </HideOnErrorPage>
+                        
+                        <Routes>
+                            <Route index element={<HomePage /> } />
+                            <Route path="/workout" element={<WorkoutPage />} />                         
+                            <Route path="/register" element={<RegisterPage /> } />
+                            <Route path="/login" element={<LoginPage /> } />
+                            <Route path="/profile" element={<ProfilePage /> } />
+                            <Route path="/coach" element={ <CoachPage />} />
+                            <Route path="/savedWorkouts" element={<SavedWorkoutsPage /> } />
+                            <Route path='*' element={<ErrorPage /> } />
+                        </Routes>
+                    </Suspense>
+                </BrowserRouter>
+                </AuthProvider>
+            </QueryClientProvider>
+        </Provider>
+    );
+}
+
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
-    <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-            <AuthProvider>
-            <BrowserRouter>
-                <Suspense>
-                    <Navbar/>
-                    <Routes>
-                        <Route path="/" element={<HomePage /> } />
-                        <Route path="/workout" element={<WorkoutPage />} />                         
-                        <Route path="/register" element={<RegisterPage /> } />
-                        <Route path="/login" element={<LoginPage /> } />
-                        <Route path="/profile" element={<ProfilePage /> } />
-                        <Route path="/coach" element={ <CoachPage />} />
-                        <Route path="/savedWorkouts" element={<SavedWorkoutsPage /> } />
-                        <Route element={<ErrorPage /> } />
-                    </Routes>
-                </Suspense>
-            </BrowserRouter>
-            </AuthProvider>
-        </QueryClientProvider>
-    </Provider>
+    <App />
 );
