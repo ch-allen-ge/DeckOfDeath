08f201eec202a04dfac204e7ff70b2f7c66fb0e4
Allen Tan <d4rkmage80@gmail.com>
2024-04-15 12:24:46 -0400

    creating custom card instead of loading from img

--- src/components/CurrentCard/CurrentCard.tsx
+++ src/components/CurrentCard/CurrentCard.tsx
@@ -8,11 +8,54 @@ interface CurrentCardProps {
     }
 }
 
+const getCardValue = (value: string) => {
+    if (!Number.isNaN(parseInt(value))) {
+        return value;
+    } else if (value === 'JACK') {
+        return 'J';
+    } else if (value === 'QUEEN') {
+        return 'Q';
+    } else if (value === 'KING') {
+        return 'K';
+    } else {
+        return '';
+    }
+};
+
+const getCardSuit = (suit: string) => {
+    switch (suit) {
+        case 'SPADES':
+            return '♠';
+        case 'HEARTS':
+            return '♥';
+        case 'CLUBS':
+            return '♣';
+        case 'DIAMONDS':
+            return '♦';
+        default:
+            return '';
+    }
+};
+
 const CurrentCard = ({currentCard} : CurrentCardProps) => {
-    const code = currentCard.code;
+    const suitColor = ((currentCard.suit === 'HEARTS' || currentCard.suit === 'DIAMONDS') ? 'suit-red' : 'suit-black');
+    const cardValue = getCardValue(currentCard.value);
+    const cardSuit = getCardSuit(currentCard.suit);
     return (
-        <div className='currentCardContainer'>
-            <img src={`/images/cards/${code}.png`} className='currentCardImg' alt={code}></img>
+        <div className="currentCardContainer">
+            <div className="rectangle front-card">
+                <div className="left-container">
+                    <span className={`num-left ${suitColor}`}>{cardValue}</span>
+                    <span className={`suit-left ${suitColor}`}>{cardSuit}</span>
+                </div>
+
+                <div className={`face-image  ${suitColor}`}>{cardSuit}</div>
+
+                <div className="right-container">
+                    <span className={`num-right ${suitColor}`}>{cardValue}</span>
+                    <span className={`suit-right ${suitColor}`}>{cardSuit}</span>
+                </div>
+            </div>
         </div>
     );
 }
